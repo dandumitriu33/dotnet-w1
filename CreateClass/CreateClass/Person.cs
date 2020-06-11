2a0b98d9a2cbbe9856aea533c90b27d84014f787
Dan Dumitriu <dandumitriu33@gmail.com>
2020-06-11 14:31:35 +0300

    Add Create New Person exercise

---
+++ CreateClass/CreateClass/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public Person(string name, DateTime birthday)
+        {
+            Name = name;
+            Birthday = birthday;
+        }
+
+        enum gender 
+        {
+            Male,
+            Female
+        }
+
+        public override string ToString()
+        {
+            return $"The person's name is {Name} and the birthday is {Birthday.ToString()}";
+        }
+    }
+}
---
+++ CreateClass/CreateClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gigi = new Person("Gigi", new DateTime(04 / 05 / 2001));
+
+            gigi.Birthday = new DateTime(2001, 04, 01);
+
+            Console.WriteLine(gigi.ToString());
+        }
+    }
+}
